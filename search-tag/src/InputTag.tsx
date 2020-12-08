d6c496acb7bf7a8bd8ba211c11d8c81928acdf1d
dowap2 <dowapdowari@gmail.com>
2020-12-08 16:29:32 +0900

    add text item

--- search-tag/src/InputTag.tsx
+++ search-tag/src/InputTag.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Input = styled.input`
-  width: 50px;
+  width: 400px;
 `;
 
 export function InputTag() {
+  const [tag, setTag] = useState<any>([]);
+  const [value, setValue] = useState<string>("");
+  const [index, setIndex] = useState<number>(0);
+  function pressFunc(key: any, tagName: string) {
+    const tagItem = [<div>{tagName}</div>];
+    if (key === "Enter") {
+      setTag(tag.concat(tagItem));
+      setValue("");
+      setIndex(index + 1);
+    }
+  }
   return (
     <div>
-      <input type="text" />
+      <Input
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        onKeyPress={e => pressFunc(e.key, value)}
+      />
+      <button onClick={e => console.log(tag)}></button>
+      {tag}
     </div>
   );
 }
