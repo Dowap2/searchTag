3dae89d0eb8e15e2e679ce45a60b050675f800d2
dowap2 <dowapdowari@gmail.com>
2020-12-09 19:51:39 +0900

    delete unnessery file

--- search-tag/src/App.tsx
+++ search-tag/src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { InputTag } from "./InputTag";
 
 function App() {
--- search-tag/src/InputTag.tsx
+++ search-tag/src/InputTag.tsx
@@ -1,22 +1,32 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const Input = styled.input`
   width: 400px;
 `;
-
 export function InputTag() {
   const [tag, setTag] = useState<any>([]);
   const [value, setValue] = useState<string>("");
   const [index, setIndex] = useState<number>(0);
+
   function pressFunc(key: any, tagName: string) {
-    const tagItem = [<div>{tagName}</div>];
+    const tagItem = [
+      <div>
+        {tagName}
+        <button onClick={e => deleteFunc(tag.length)}>x</button>
+      </div>
+    ];
     if (key === "Enter") {
       setTag(tag.concat(tagItem));
       setValue("");
       setIndex(index + 1);
     }
   }
+
+  function deleteFunc(len: number) {
+    console.log(tag);
+  }
+
   return (
     <div>
       <Input
@@ -25,7 +35,6 @@ export function InputTag() {
         onKeyPress={e => pressFunc(e.key, value)}
       />
       <button onClick={e => console.log(tag)}></button>
-      {tag}
     </div>
   );
 }
