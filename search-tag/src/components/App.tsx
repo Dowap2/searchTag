04d55cde812b7b66753a3de93b4f2ab60e8de442
dowap2 <dowapdowari@gmail.com>
2021-03-24 05:01:16 +0900

    add darkmode

--- search-tag/src/components/App.tsx
+++ search-tag/src/components/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
 import { TagComponent } from "../components/TagComponents";
 import { BoxComponent } from "../ui/BoxComponent";
 
 function App() {
-  console.log("dsfd");
+  const Box = styled.div`
+    background: ${(props: any) => props.color};
+  `;
+  const mode = useSelector((state: any) => state.tagState.state.mode);
   return (
-    <div>
+    <Box color={mode === "light" ? "#ffffff" : "#000000"}>
       <TagComponent />
-    </div>
+      <button onClick={e => console.log(mode)}>sdafafs</button>
+    </Box>
   );
 }
 
